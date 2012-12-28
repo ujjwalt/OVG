48842e2ef2d8a7d9fad9687533a4122b96901178
Karimkhan <karimkhan_it@yahoo.com>
2012-12-28 13:04:07 +0530

    Update src/main/server/server.go
    In progress ..

--- src/main/server/server.go
+++ src/main/server/server.go
@@ -1,6 +1,33 @@
-// Server module
+//Server will manage the communication with all host-replacing bad host-credit management
 package main
+func main(){
 
-func main() {
-    
+getHostList(); 
+
+sendMessage();
+
+recMessage();
+checkAlive();
+replaceWorker();
+}
+
+func getHostList() []string {
+//On server details about all the hosts would be there, so fetch host list and return
+func (db *DB) QueryRow(query string, args ...interface{}) *Row
+//QueryRow executes a query that is expected to return at most one row.
+var s NullString
+err := db.QueryRow("SELECT id FROM db WHERE", id).Scan(&s)
+if s.Valid {
+   // use s.String
+} else {
+   // NULL value
+}
+// Scan scans text read from standard input, storing successive
+// space-separated values into successive arguments.  Newlines count
+// as space.  It returns the number of items successfully scanned.
+//return s
+**}**
+
+func sendMessage() []string {
+
 }
