9e98f1f48e968070cf075ebcc53e20127c8d2164
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2012-12-28 21:03:15 +0530

    Added username and password flag parsing to start with

--- src/main/client/client.go
+++ src/main/client/client.go
@@ -1,6 +1,23 @@
 // Client module
 package main
 
+import (
+	"flag"
+	//"fmt"
+	"log"
+	//"os"
+)
+
+var (
+	uname  = flag.String("u", "", "Enter your username")
+	passwd = flag.String("p", "", "Enter your password")
+)
+
 func main() {
-    
+	// Parse the username and password from the flags
+	flag.Parse()
+	if *uname == "" || *passwd == "" {
+		log.Fatal("Invalid username or password: ", *uname)
+	}
+	// 
 }
