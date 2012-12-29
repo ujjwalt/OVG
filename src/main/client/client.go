6ad7de64d5f3139f95d3c073e5e0f563536af074
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2012-12-29 19:38:49 +0530

    Creation and reading and writing to config.json works

--- src/main/client/client.go
+++ src/main/client/client.go
@@ -21,34 +21,22 @@ import (
 	"crypto/md5"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
-	// "path"
-	"fmt"
+	"os/user"
 )
 
 var (
-	uname   = flag.String("u", "username", "Enter your username")
-	passwd  = flag.String("p", "password", "Enter your password")
-	config  configT
-	configF *os.File
-)
-
-const (
-	configPath          = `~/Library/OVG/config.json`
-	emptyConfigTemplate = `{
-        "username" : "username",
-        "password" : "password_as_md5",
-        "projects" :
-        [
-            {
-                "id"    : "project_id",
-                "folder": "path/to/the/folder/containing/files",
-                "cpu"   : cycles_spent on this project
-            }
-        ]
-    }`
+	uname          = flag.String("u", "username", "Enter your username")
+	passwd         = flag.String("p", "password", "Enter your password")
+	config         configT  // memory model of config.json
+	configF        *os.File // file pointer to config.json
+	currentUser, _ = user.Current()
+	libPath        = currentUser.HomeDir + "/Library/OVG/"
+	configPath     = libPath + "config.json"
+	err            error // generic error
 )
 
 type projectT struct {
@@ -58,9 +46,9 @@ type projectT struct {
 }
 
 type configT struct {
-	username string
-	password string
-	projects []projectT
+	Username string
+	Password string
+	Projects []projectT
 }
 
 func main() {
@@ -69,36 +57,47 @@ func main() {
 	if *uname == "" || *passwd == "" {
 		log.Fatal("Invalid username or password: ", *uname)
 	}
-	configF, err := os.Open(configPath) // only readable and writable by current user
+	// create the lib directory
+	fatal(os.MkdirAll(libPath, 0700))
+	fatal(os.Chdir(libPath))
+	configF, err = os.OpenFile(configPath, os.O_RDWR, 0644) // only readable and writable by current user
 	if err != nil {
-		// file does not exist to create an empty one
-		configF, err = os.Create(configPath)
-		if err != nil {
-			log.Fatal(err)
-		}
+		configF, err = os.OpenFile(configPath, os.O_RDWR|os.O_CREATE, 0644)
+		fatal(err)
+		/*_, err = configF.WriteString(emptyConfigTemplate) // write out th empty template
+		fatal(err)*/
+	} else {
+		readConfig()
 	}
-	configF.WriteString(emptyConfigTemplate) // write out th empty template
-	info, err := configF.Stat()
-	if err != nil {
-		log.Fatal(err)
-	}
-	b := make([]byte, info.Size())
-	configF.Read(b)
-	err = json.Unmarshal(b, &config)
-	if err != nil {
-		log.Fatal(err)
-	}
+	defer configF.Close()
 	// store the username and password
-	config.username = *uname
+	config.Username = *uname
 	h := md5.New() // convert the password to md5
 	io.WriteString(h, *passwd)
-	config.password = fmt.Sprintf("%x", h.Sum(nil))
-	b, err = json.Marshal(config)
+	config.Password = fmt.Sprintf("%x", h.Sum(nil))
+	writeConfig()
+}
+
+func fatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = configF.Write(b)
-	if err != nil {
-		log.Fatal(err)
-	}
+}
+
+func writeConfig() {
+	b, err := json.Marshal(config)
+	fatal(err)
+	fatal(configF.Truncate(0))
+	_, err = configF.WriteAt(b, 0)
+	fatal(err)
+	fatal(configF.Sync()) // write to stable storage
+}
+
+func readConfig() {
+	info, err := configF.Stat()
+	fatal(err)
+	b := make([]byte, info.Size())
+	_, err = configF.Read(b)
+	fatal(err)
+	fatal(json.Unmarshal(b, &config))
 }
