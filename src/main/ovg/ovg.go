f1a9b827e3881531b52d0f2d4a0f07ba9213cb2d
Karimkhan <karimkhan_it@yahoo.com>
2013-01-15 06:33:00 -0500

    Updated ovg.go to integrate with mongodb
    Giving error in executing mongodb statements.
    Register(), NewWorker(), WorkerDead(), message() methods are added to handle and communicate with host.

--- src/main/ovg/ovg.go
+++ src/main/ovg/ovg.go
@@ -1,39 +1,26 @@
 package ovg
 
 type ProjectNotifier interface {
+
 	/*
 	   This call indicates that a particular worker has become unresponsive. The arguments passed are :-
 	       * deadWorker- The id of the worker who has gone dead.
 	       * replaced - this indicates wether the dead worker could be replaced with another one or not. If the worker is replaced then
 	           it retains the same id i.e. deadWorker
 	*/
+	
 	WorkerDead(deadWorker int, replaced bool)
-
+	
 	/*
 	   This call signals that a new worker is available for this project and should be added to the list of workers. The single argument
 	   newWorker is the id assigned to this worker. This id can be one previously assigned to a dead worker that could not be replaced.
 	*/
-	NewWorker(newWorker int)
+
+	NewWorker(newWorker int, project_id string, ip int)
 }
 
-// The type of object returned by the server on registeration
-type serverDelegate struct {
-	// the number of workers assigned initially to the project
-	workers int
-	// id of the project this delegate represents
-	projectID string
-}
-
-func (s serverDelegate) Workers() int {
-	return s.workers
-}
-
-func (s serverDelegate) Message(worker int, message string, arg interface{}, reply interface{}) chan bool {
-	return s.workers
-}
-
-// This is the type of objects
 type ServerDelegator interface {
+
 	/*
 	   This call will be passed a single integer argument which is the count of workers available for the project.
 	   The ids assigned to these workers will be 0 to worker-1 e.g. if workers == 10 then the ids' assigned to these 10 workers are 0,1,...9.
@@ -54,11 +41,48 @@ type ServerDelegator interface {
 
 	   A single result is returned - a singly buffered boolean channel which signals wether the request succeeded or not.
 	*/
+
 	Message(worker int, message string, arg interface{}, reply interface{}) chan bool
+}
+
+// The type of object returned by the server on registeration
+type serverDelegate struct {
+	
+	workers int
+	projectID string
+}
+
+type projectNotify struct {
+
+	workers int
+	projectID string
+	ip int
+}
+
+func (s serverDelegate) Workers() int {
+	var cnt int
+	cnt := db.serverdb.distinct('workers')
+	return cnt
+}
+
+func (s serverDelegate) Message(worker int, message string, arg interface{}, reply interface{}) chan bool {
+
+	// Code to send message to host.
+	//Why return workers ?
+	return s.workers
 }
 
 // This function registers the project notifier provided by the project and returns a ServerDelegator that the project can use
 // to fetch important data and perform critical functions like messaging
-func Register(p ProjectNotifier) ServerDelegator {
 
+
+func Register(p ProjectNotify) ServerDelegator {
+	db.serverdb.insert({p.workers, p.projectID, p.ip})		
+}
+	
+func (p ProjectNotifier) WorkerDead(deadWorker int, replaced bool) {
+	
+} 
+func (p ProjectNotifier) NewWorker(newWorker int, project_id string, ip int) {
+	db.serverdb.insert({p.workers, p.projectID, p.ip})		
 }
